ac55263e7d8828766a08a2367f31e608dd34dfd6
alpillow <54637901+alpillow@users.noreply.github.com>
2019-08-28 19:51:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = 
+    propt ("What is your first name?");
+var lastName =
+    propt ("What is your last name?");
+
+document.body.querySelector ("#box1").innerHTML=firstName;
+
+document.body.querySelector
+("box2").innerHTML=lastName;
